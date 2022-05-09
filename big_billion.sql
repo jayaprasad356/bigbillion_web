928edd3a0d83dad78f2a3f57401444dd970bf983
Divaharsha <divakarvan03@gmail.com>
2022-05-09 14:15:29 -0700

    sql file added

--- big_billion.sql
+++ big_billion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 08, 2022 at 08:47 AM
+-- Generation Time: May 09, 2022 at 11:14 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -24,6 +24,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `games`
+--
+
+CREATE TABLE `games` (
+  `id` int(11) NOT NULL,
+  `user_id` text DEFAULT NULL,
+  `game_name` text DEFAULT NULL,
+  `game_type` text DEFAULT NULL,
+  `game_method` text NOT NULL,
+  `number` text DEFAULT NULL,
+  `points` text DEFAULT NULL,
+  `last_updated` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `games`
+--
+
+INSERT INTO `games` (`id`, `user_id`, `game_name`, `game_type`, `game_method`, `number`, `points`, `last_updated`, `date_created`) VALUES
+(3, NULL, 'jodi', 'today games', 'cross', '07', '100', NULL, '2022-05-09 21:06:00'),
+(4, '1', 'jodi', 'today games', 'cross', '07', '100', NULL, '2022-05-09 21:06:46'),
+(5, '1', 'jodi', 'today games', 'cross', '07', '100', NULL, '2022-05-09 21:07:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `points`
+--
+
+CREATE TABLE `points` (
+  `id` int(11) NOT NULL,
+  `user_id` text DEFAULT NULL,
+  `points` text DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `points`
+--
+
+INSERT INTO `points` (`id`, `user_id`, `points`, `date_created`) VALUES
+(1, '1', '50', '2022-05-09 04:45:55'),
+(2, '1', '50', '2022-05-09 04:51:27'),
+(3, '1', '50', '2022-05-09 20:55:53'),
+(4, '1', '50', '2022-05-09 20:57:00');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,7 +81,11 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` text DEFAULT NULL,
   `mobile` text DEFAULT NULL,
+  `account_number` text DEFAULT NULL,
+  `ifsc_code` text DEFAULT NULL,
+  `holder_name` text DEFAULT NULL,
   `points` text DEFAULT NULL,
+  `earn` text DEFAULT NULL,
   `last_updated` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
   `date_created` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -40,12 +94,44 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `mobile`, `points`, `last_updated`, `date_created`) VALUES
-(1, 'hello', '7358832695', '5', '2022-05-08 06:45:24', '2022-05-08 04:21:04');
+INSERT INTO `users` (`id`, `name`, `mobile`, `account_number`, `ifsc_code`, `holder_name`, `points`, `earn`, `last_updated`, `date_created`) VALUES
+(1, 'hello', '7358832695', '345678', 'bdg6327', 'divakar', '200', '350', '2022-05-09 20:57:00', '2022-05-08 04:21:04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `withdrawal`
+--
+
+CREATE TABLE `withdrawal` (
+  `id` int(11) NOT NULL,
+  `user_id` text DEFAULT NULL,
+  `amount` text DEFAULT NULL,
+  `date_created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `withdrawal`
+--
+
+INSERT INTO `withdrawal` (`id`, `user_id`, `amount`, `date_created`) VALUES
+(1, '1', '100', '2022-05-09 18:04:00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `games`
+--
+ALTER TABLE `games`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `points`
+--
+ALTER TABLE `points`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -54,13 +140,37 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `withdrawal`
+--
+ALTER TABLE `withdrawal`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `games`
+--
+ALTER TABLE `games`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `points`
+--
+ALTER TABLE `points`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `withdrawal`
+--
+ALTER TABLE `withdrawal`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
