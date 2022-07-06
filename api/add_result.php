b116bd65c7af90c9f60f23b6a342456c6e14c2af
jayaprasad356 <jayaprasad356@gmail.com>
2022-07-06 20:52:12 +0530

    add result

--- api/add_result.php
+++ api/add_result.php
@@ -70,7 +70,30 @@ foreach ($res as $row) {
     $sql = "INSERT INTO `transactions` (user_id,points,balance,type,game_name,date,date_created) VALUES('$user_id','$points','$update_user_points','correctresult','$game_name','$date','$datetime')" ;
     $db->sql($sql);
 }
-$sql = "SELECT * FROM haruf WHERE game_date = '$date' AND game_name = '$game_name' AND number = '$result'";
+$harufresult = sprintf("%02d", $result);
+$andarresult = substr($harufresult, 0, 1); 
+$baharresult = substr($harufresult, 1, 2); 
+$sql = "SELECT * FROM haruf WHERE game_date = '$date' AND game_name = '$game_name' AND number = '$andarresult' AND game_type = 'andar";
+$db->sql($sql);
+$res = $db->getResult();
+foreach ($res as $row) {
+    $points = round($row['points'] * 9.5);
+    $user_id = $row['user_id'];
+    $sql = "INSERT INTO winners (user_id, points, game_name,date,result) VALUES ('$row[user_id]', '$points', '$game_name', '$date','$result')";
+    $db->sql($sql);
+    $sql = "UPDATE users SET points = points + '$points' WHERE id = '$row[user_id]'";
+    $db->sql($sql);
+    $sql = "SELECT * FROM users WHERE id = '$row[user_id]'";
+    $db->sql($sql);
+    $res = $db->getResult();
+    $update_user_points = $res[0]['points'];
+    $datetime = Date('Y-m-d H:i:s');
+    $date = date('Y-m-d');
+    $sql = "INSERT INTO `transactions` (user_id,points,balance,type,game_name,date,date_created) VALUES('$user_id','$points','$update_user_points','correctresult','$game_name','$date','$datetime')" ;
+    $db->sql($sql);
+
+}
+$sql = "SELECT * FROM haruf WHERE game_date = '$date' AND game_name = '$game_name' AND number = '$baharresult' AND game_type = 'bahar";
 $db->sql($sql);
 $res = $db->getResult();
 foreach ($res as $row) {
