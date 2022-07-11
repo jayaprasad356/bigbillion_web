e5768fc3e8d47bb098bf9e52759abd6128349e8f
jayaprasad356 <jayaprasad356@gmail.com>
2022-07-12 00:49:40 +0530

    haruf bids

--- api/allharufbids.php
+++ api/allharufbids.php
@@ -25,20 +25,27 @@ if (empty($_POST['game_name'])) {
 }
 $date = $db->escapeString($_POST['date']);
 $game_name = $db->escapeString($_POST['game_name']);
-$sql = "SELECT *,SUM(points) AS points FROM `haruf` WHERE game_date = '$date' AND game_name = '$game_name' GROUP BY number ORDER BY number DESC";;
+$sql = "SELECT *,SUM(points) AS points FROM `haruf` WHERE game_type='andar' AND  game_date = '$date' AND game_name = '$game_name' GROUP BY number ORDER BY number DESC";;
 $db->sql($sql);
-$res = $db->getResult();
-$num = $db->numRows($res);
+$res1 = $db->getResult();
+$num1 = $db->numRows($res1);
+$sql = "SELECT *,SUM(points) AS points FROM `haruf` WHERE game_type='bahar' AND  game_date = '$date' AND game_name = '$game_name' GROUP BY number ORDER BY number DESC";;
+$db->sql($sql);
+$res2 = $db->getResult();
+$num2 = $db->numRows($res2);
+$row = array();
+$row = array_merge($res1, $res2);
+$num = count($row);
 if ($num >= 1) {
     $response['success'] = true;
     $response['message'] = "Bids listed Successfully";
-    $response['data'] = $res;
+    $response['data'] = $row;
     print_r(json_encode($response));
 }
 else{
     $response['success'] = false;
     $response['message'] = "No Bids Found";
-    $response['data'] = $res;
+    $response['data'] = $row;
     print_r(json_encode($response));
 
 }
