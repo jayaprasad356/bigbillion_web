cd73a4f5c02758c50371f3062d8a6b6d73343111
jayaprasad356 <jayaprasad356@gmail.com>
2022-08-08 22:52:49 +0530

    user deactivated

--- api/updateuser.php
+++ api/updateuser.php
@@ -36,7 +36,7 @@ $mobile = $db->escapeString($_POST['mobile']);
 $name = $db->escapeString($_POST['name']);
 $device_id = $db->escapeString($_POST['device_id']);
 $datetime = Date('Y-m-d H:i:s');
-$sql= "INSERT INTO users (mobile,name,points,loggedin,device_id,date_created) VALUES ('$mobile','$name',0,1,'$device_id','$datetime')";
+$sql= "INSERT INTO users (mobile,name,points,loggedin,device_id,user_status,date_created) VALUES ('$mobile','$name',0,1,'$device_id',1,'$datetime')";
 $db->sql($sql);
 $res = $db->getResult();
 $sql = "SELECT * FROM users WHERE mobile ='$mobile'";
