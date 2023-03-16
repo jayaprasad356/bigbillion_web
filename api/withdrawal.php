d3ae4ef2b8dcd3336b549eb59843a76914ad20e9
jayaprasad356 <jayaprasad356@gmail.com>
2023-03-16 16:22:56 +0530

    withdrawals

--- api/withdrawal.php
+++ api/withdrawal.php
@@ -31,39 +31,32 @@ $db->sql($sql);
 $res = $db->getResult();
 $num = $db->numRows($res);
 if ($num == 1) {
-    if($amount >= 100){
+    $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
+    $points=$res[0]['points'];
+    if($points>=$amount){
+        $datetime = Date('Y-m-d H:i:s');
+        $date = date('Y-m-d');
+        $new_earn=$points-$amount;
+        $sql = "UPDATE `users` SET `points`='$new_earn' WHERE id=" . $user_id;
+        $db->sql($sql);
+        $sql = "INSERT INTO withdrawal  (user_id,points,status,date,date_created) VALUES ('$user_id','$amount',0,'$date','$datetime')";
+        $db->sql($sql);
         $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
-        $points=$res[0]['points'];
-        if($points>=$amount){
-            $datetime = Date('Y-m-d H:i:s');
-            $date = date('Y-m-d');
-            $new_earn=$points-$amount;
-            $sql = "UPDATE `users` SET `points`='$new_earn' WHERE id=" . $user_id;
-            $db->sql($sql);
-            $sql = "INSERT INTO withdrawal  (user_id,points,status,date,date_created) VALUES ('$user_id','$amount',0,'$date','$datetime')";
-            $db->sql($sql);
-            $sql = "SELECT * FROM users WHERE id = '" . $user_id . "'";
-            $db->sql($sql);
-            $res = $db->getResult();
-            $datetime = Date('Y-m-d H:i:s');
-            $date = date('Y-m-d');
-            $sql = "INSERT INTO `transactions` (user_id,points,balance,type,date,date_created) VALUES('$user_id','$amount','$new_earn','withdrawal','$date','$datetime')" ;
-            $db->sql($sql);
-            $response['success'] = true;
-            $response['message'] = "Amount withdrawed Requested  Successfully";
-            $response['data'] = $res;
-        }
-        else{
-            $response['success'] = false;
-            $response['message'] = "Insufficient Fund";
-    
-        }
-        
-    }else{
-        $response['success'] = false;
-        $response['message'] = "Minimum Withdrawal is 100";       
+        $db->sql($sql);
+        $res = $db->getResult();
+        $datetime = Date('Y-m-d H:i:s');
+        $date = date('Y-m-d');
+        $sql = "INSERT INTO `transactions` (user_id,points,balance,type,date,date_created) VALUES('$user_id','$amount','$new_earn','withdrawal','$date','$datetime')" ;
+        $db->sql($sql);
+        $response['success'] = true;
+        $response['message'] = "Amount withdrawed Requested  Successfully";
+        $response['data'] = $res;
     }
+    else{
+        $response['success'] = false;
+        $response['message'] = "Insufficient Fund";
 
+    }
 
 }
 else{
