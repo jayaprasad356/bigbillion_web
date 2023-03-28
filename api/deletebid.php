905f0857f1e4271e13afa4609d98c1f8ccf83886
jayaprasad356 <jayaprasad356@gmail.com>
2023-03-28 20:06:15 +0530

    delete bid

--- api/deletebid.php
+++ api/deletebid.php
@@ -31,9 +31,38 @@ if (empty($_POST['game_name'])) {
     print_r(json_encode($response));
     return false;
 }
+
 $date = $db->escapeString($_POST['date']);
 $game_name = $db->escapeString($_POST['game_name']);
 $user_id = $db->escapeString($_POST['user_id']);
+
+
+if($game_name =='FD'){
+    $game_time = '05:45 PM';
+}
+else if($game_name =='GB'){
+    $game_time = '07:45 PM';
+}
+else if($game_name =='GL'){
+    $game_time = '10:45 PM';
+}
+else if($game_name =='DS'){
+    $game_time = '02:20 AM';
+}
+
+$start_date = $date .' '.$game_time;
+$start_date = new DateTime($start_date);
+$now = new DateTime();
+$now  = $now->format('Y-m-d h:i A');
+$end_date = new DateTime($now);
+
+if ($start_date < $end_date) {
+    //$game_date = date('Y-m-d', strtotime($game_date . ' +1 day'));
+    $response['success'] = false;
+    $response['message'] = "GAME OVER";
+    print_r(json_encode($response));
+    return false;
+}
 $sql = "SELECT * FROM games WHERE user_id = '$user_id' AND game_name = '$game_name' AND game_date = '$date'";
 $db->sql($sql);
 $res = $db->getResult();
